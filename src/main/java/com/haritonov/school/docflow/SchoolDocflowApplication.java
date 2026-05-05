078a664543592f8ae33bb3d8b3bf61714028ef39
roguelike <nikita.harit@mail.ru>
2026-05-05 09:35:42 +0300

    Initial commit

---
+++ src/main/java/com/haritonov/school/docflow/SchoolDocflowApplication.java
@@ -0,0 +1,13 @@
+package com.haritonov.school.docflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolDocflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolDocflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haritonov/school/docflow/SchoolDocflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haritonov.school.docflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolDocflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
