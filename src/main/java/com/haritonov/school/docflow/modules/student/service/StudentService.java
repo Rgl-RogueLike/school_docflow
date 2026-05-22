89d57abf23ba73580ad6c38fd1df687b23a14ac1
roguelike <nikita.harit@mail.ru>
2026-05-22 18:27:26 +0300

    feat(students): add filtering for students list

--- src/main/java/com/haritonov/school/docflow/modules/student/service/StudentService.java
+++ src/main/java/com/haritonov/school/docflow/modules/student/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.haritonov.school.docflow.modules.student.service;
 
 import com.haritonov.school.docflow.modules.student.dto.StudentCreateRequest;
+import com.haritonov.school.docflow.modules.student.dto.StudentFilterDto;
 import com.haritonov.school.docflow.modules.student.dto.StudentResponse;
 import com.haritonov.school.docflow.modules.student.dto.StudentUpdateRequest;
 
@@ -19,4 +20,6 @@ public interface StudentService {
     void delete(Long id);
 
     List<StudentResponse> getByClassId(Long classId);
+
+    List<StudentResponse> getAllWithFilter(StudentFilterDto filter);
 }
